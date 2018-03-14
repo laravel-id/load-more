0c6ef91ede693e10a472c441d85896f3497b4715
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-03-14 18:11:53 +0700

    Add tutorial link

--- resources/assets/js/components/user/Index.vue
+++ resources/assets/js/components/user/Index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
+
+      <div class="col-md-8 offset-md-2">
+        <div class="alert alert-info">
+          Tutorial pembuatan fitur ini dapat dibaca pada tulisan <a href="https://wp.me/p7UAso-7H">Membuat Fitur "Load More" di Laravel Menggunakan VueJS</a>.
+        </div>
+
       <div class="card card-default">
         <div class="card-header"><h3>Users</h3></div>
         <div class="card-body no-padding">
@@ -26,6 +32,7 @@
         <div v-if="users.next_page_url" class="card-footer">
           <button @click.prevent="paginate(users.next_page_url)" type="button" class="btn btn-primary btn-block">Load More</button>
         </div>
+      </div>
       </div>
     </div>
   </div>
